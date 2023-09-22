9d6e0ebc519a92cf51a35c1a5e17444bfa4655f6
Mike <super.mvk@yandex.ru>
2023-09-22 23:47:19 +0000

    clean mess

--- src/pages/Home.page.tsx
+++ src/pages/Home.page.tsx
@@ -1,5 +1,3 @@
-import { Welcome } from '../components/Welcome/Welcome';
-import { ColorSchemeToggle } from '../components/ColorSchemeToggle/ColorSchemeToggle';
 import {Grader} from '../components/Grader/Grader'
 
 export function HomePage() {
